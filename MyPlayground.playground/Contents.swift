f0655703f9da32327ed0b55aa8f8732741b0da75
Ahmadreza <mail.sarafraz@gmail.com>
2023-09-30 10:14:04 +0330

    Extracted zip playground file

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,92 @@
+import Foundation
+
+
+let test = Test(name: "some string", time: 4, array: ["blah blah 1", "blah blah 2"])
+let url = URL(string: test.string.description)!
+let params = url.getParams()
+print("parsed response", params)
+print(stringingHelper.getType(for: "time", in: params))
+print(stringingHelper.getInt(for: "time", in: params) ?? "?")
+
+struct Test: Codable, Stringing {
+    
+    var name: String
+    var time: Int
+    var array: [String]
+    
+    var queryItems: [URLQueryItem] {
+        return stringingHelper.queryItems(params: ["name": name, "time": time.description, "array": array.joined(separator: ",")])
+    }
+    
+    var string: String {
+        return stringingHelper.string(components: queryItems)
+    }
+}
+
+extension URL {
+    
+    func getParams()-> [String: String] {
+        var dic: [String: String] = [:]
+        if let urlComponents = components {
+            let params = (urlComponents.queryItems ?? [])
+            for item in params {
+                if let val = item.value {
+                    dic[item.name] = val
+                }
+            }
+        }
+        return dic
+    }
+    
+    var components: URLComponents? {
+        return URLComponents(url: self, resolvingAgainstBaseURL: false)
+    }
+}
+
+class stringingHelper {
+    
+    static func string(components: [URLQueryItem]?)-> String {
+        var comp = URLComponents()
+        comp.scheme = "https"
+        comp.queryItems = components
+        return comp.url?.absoluteString ?? "?"
+    }
+    
+    static func queryItems(params: [String: String])-> [URLQueryItem] {
+        var items = [URLQueryItem]()
+        for (key, value) in params {
+            items.append(URLQueryItem(name: key, value: value))
+        }
+        return items
+    }
+    
+    enum VariableType { case int, double, bool, string, stringArray }
+    static func getType(for key: String, in params: [String : String])-> VariableType {
+        guard let value = params[key] else { return .string }
+        if let _ = Int(value) {
+            return .int
+        } else if let _ = Double(value) {
+            return .double
+        } else if let _ = Bool(value) {
+            return .bool
+        } else if !value.components(separatedBy: ",").isEmpty {
+            print("shit", value.components(separatedBy: ","))
+            return .stringArray
+        } else {
+            return .string
+        }
+    }
+    
+    static func getInt(for value: String, in params: [String : String])-> Int? {
+        if let val = params[value], let num = Int(val) {
+            return num
+        }
+        return nil
+    }
+}
+
+/// MARK: This protocol is a dependency inversion for using stringing functionalities using stringingHelper
+protocol Stringing {
+    var queryItems: [URLQueryItem] { get }
+    var string: String { get }
+}
